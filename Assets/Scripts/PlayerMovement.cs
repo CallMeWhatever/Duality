621b148f407b6591a8097e35c19fba5df56611e1
threeheadedshark <117372588+threeheadedshark@users.noreply.github.com>
2024-08-10 00:35:08 +0200

    Fixed some jump issues

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D body;
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D body;
+    private BoxCollider2D boxCollider;
+
 
 
     private void Awake(){
         body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update(){
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed ,body.velocity.y);
 
-        if(Input.GetKey(KeyCode.Space)){
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if(Input.GetKey(KeyCode.Space) && isGrounded()  && !touchesWall()){
+            Jump();
         }
+    }
+
+    private void Jump(){
+        body.velocity = new Vector2(body.velocity.x, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D Collision){
+    }
+
+    private bool isGrounded(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool touchesWall(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.right, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }
